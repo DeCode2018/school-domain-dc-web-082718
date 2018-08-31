1947ae8b507e82df5c4d755c9ac822bad66d206d
DeCode2018 <decode2018@gmail.com>
2018-08-31 19:28:39 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require "pry"
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+
+  def add_student(name,grade)
+     @roster[grade]||= [] #if @roster[grade] is doesnt exist, then create it.
+     @roster[grade]<<name #when @roster[grade] does exist, push value name to it
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted={}
+    roster.each do |grade, student|
+      sorted[grade] = student.sort
+    end
+    sorted
+  end
+
+end
